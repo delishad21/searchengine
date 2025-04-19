b29eae8941b271c0efdebbc549d72f331d94f423
delishad21 <soojohan@gmail.com>
2025-04-19 20:00:29 +0800

    Add comments to search code

--- search/search_engine.py
+++ search/search_engine.py
@@ -10,27 +10,36 @@ def search(query, db_path="../search_index.db", max_results=50):
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
-    # Get term frequencies
+    # For each term in the query:
+    # Fetch all pages where the term appears (from the keywords table).
+    # Store:
+    # Docs that the term appears in (term_docs)
+    # How many docs it appears in (doc_freq)
+
     term_docs = {}
     doc_freq = {}
 
     for term in terms:
-        cursor.execute("SELECT page_id, frequency FROM keywords WHERE word = ?", (term,))
+        cursor.execute("SELECT page_id, frequency FROM keywords WHERE word = ?", (term,)) # keywords is inverted index
         results = cursor.fetchall()
         term_docs[term] = results
         doc_freq[term] = len(set(r["page_id"] for r in results))
 
+    # Build TF Vectors for Documents
+
     doc_vectors = {}
     doc_max_tf = {}
 
-    for term, postings in term_docs.items():
+    for term, postings in term_docs.items(): # posting here refers to a document and the frequency in which the term appears
         for row in postings:
             pid, freq = row["page_id"], row["frequency"]
             doc_vectors.setdefault(pid, {})[term] = freq
             doc_max_tf[pid] = max(doc_max_tf.get(pid, 0), freq)
 
+    # Retrieve total number of pages crawled
     N = cursor.execute("SELECT COUNT(*) FROM pages").fetchone()[0]
 
+    # Calculate TF-IDF for each document (with boost from title match and phrase match)
     scores = {}
     for pid, vector in doc_vectors.items():
         doc_vec = []
@@ -60,7 +69,7 @@ def search(query, db_path="../search_index.db", max_results=50):
         if any(term in stemmed_title for term in terms):
             score *= 1.5
 
-        # Phrase boost
+        # Phrase boost if text contains phrase
         cursor.execute("SELECT GROUP_CONCAT(word, ' ') as full_text FROM keywords WHERE page_id = ?", (pid,))
         text = cursor.fetchone()["full_text"] or ""
 
